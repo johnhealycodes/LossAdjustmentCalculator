4ac5285f4388acdb1f2e48cc45e840960a4faf0e
johnhealycodes <93292715+johnhealycodes@users.noreply.github.com>
2022-01-17 21:58:28 -0600

    added JS functionality
    having issues returning the template literal to the HTML file, type of error "Uncaught TypeError: Cannot read properties of null (reading 'innerHTML') at index.js:47:42"

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
-console.log('hello world');
+let oldRCV = document.getElementById("oldRCV").value;
+let newRCV = document.getElementById("newRCV").value;
+let depreciation = document.getElementById("depreciation").value;
+let nDepreciation = document.getElementById("nDepreciation").value;
+let deductible = document.getElementById("deductible").value;
+let priorPayment = document.getElementById("payments").value;
 
-let i = 0;
-for (let i = 1; i <= 5; i++) {
-    console.log(i)
+const supplement = newRCV - oldRCV;
+const ACV = newRCV - depreciation - nDepreciation
+const netPayment = ACV - deductible - priorPayment
+const reserve = newRCV - deductible
 
+//Order of operations
+console.log(oldRCV);
+console.log(supplement);
+console.log(newRCV);
+console.log(depreciation);
+console.log(nDepreciation);
+console.log(ACV);
+console.log(deductible);
+console.log(priorPayment);
+console.log(netPayment);
+console.log(reserve);
+    
+
+    
+
+
+
+
+//Loss Adjustment Note to Copy and Paste:
+
+function htmlLossAdjustment() {
+    return `<h1>Supplement Loss Adjustment</h1>
+    <ul>Old Replacement Cost Value ${oldRCV}</ul>
+    <ul>Supplement Amount ${supplement}</ul>
+    <ul>New Replacement Cost Value ${newRCV}</ul>
+    <ul>Less Recoverable Depreciation${depreciation}</ul>
+    <ul>Less Non-Recoverable Depreciation ${nDepreciation}</ul>
+    <ul>Supplement Actual Cash Value ${ACV}</ul>
+    <ul>Less Deductible ${deductible}</ul>
+    <ul>Less Prior Payments ${priorPayment}</ul>
+    <ul>Net Supplement Payment Amount ${netPayment}
+    <ul>Total Loss Less Deductible ${reserve}</ul>`
 }
+    
+document.getElementById("lossAdjustment").innerHTML
